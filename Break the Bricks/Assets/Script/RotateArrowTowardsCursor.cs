5648435ad3c95763ecbba3bc0174fd78ad16c62e
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-05-29 03:53:10 +0530

    Arrow Logic done

---
+++ Break the Bricks/Assets/Script/RotateArrowTowardsCursor.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class RotateArrowTowardsCursor : MonoBehaviour
+{
+    public float Angle;
+    void Update()
+    {
+        Vector2 directon = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(directon.y, directon.x) * Mathf.Rad2Deg;
+        if (angle < 30 || angle > 150)
+            return;
+        Angle = angle;
+        Quaternion rotaion = Quaternion.AngleAxis(angle,Vector3.forward);
+        transform.rotation = rotaion;
+    }
+}
