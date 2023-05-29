acd5bdbf16e15280986c216b303b304705c0ad37
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-05-29 06:35:01 +0530

    Bounce logic done

--- Break the Bricks/Assets/Script/BallController.cs
+++ Break the Bricks/Assets/Script/BallController.cs
@@ -5,27 +5,28 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     private Rigidbody2D rb;
+
+    Vector2 lastVelocity;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        CheckWallCollision();
+        lastVelocity = rb.velocity;
     }
-    private void CheckWallCollision()
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 position = transform.position;
-        Vector2 viewportPosition = Camera.main.WorldToViewportPoint(position);
-
-        if (viewportPosition.x <= 0 || viewportPosition.x >= 1)
+        if (collision.gameObject.CompareTag("Wall"))
         {
-            rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+            Debug.Log(lastVelocity);
+            float speed = lastVelocity.magnitude;
+            Vector2 direction = Vector2.Reflect(lastVelocity.normalized,collision.contacts[0].normal);
+            rb.velocity = direction * Mathf.Max(speed, 5f);
+            Debug.Log(rb.velocity);
+
         }
 
-        if (viewportPosition.y <= 0 || viewportPosition.y >= 1)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
-        }
     }
 }
